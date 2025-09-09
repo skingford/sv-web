c3dfaa0d9a493bb56870bea24777db4a3a0b4084
SteveKingFord <332312568@qq.com>
2025-09-09 23:16:13 +0800

    perf(page): remove page

---
+++ src/routes/settings/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>account: {data.id}</h1>
+<p>当前动态参数 id 的值是: <strong>{data.id}</strong></p>
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -4,3 +4,4 @@
 
 <h1>profile</h1>
 <Button>Click me</Button>
+<div class="bg-primary text-primary-foreground">Hello</div>
